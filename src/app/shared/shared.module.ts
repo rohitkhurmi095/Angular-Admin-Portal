a4d23f115cfbf1a4948f76efde29e73df11dee5b
rohit <rohit.khurmi095@gmail.com>
2024-01-22 00:48:37 +0530

    Theme Integration - Layout(1)

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,7 +4,7 @@ import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { BreadcrumbComponent } from './components/breadcrumb/breadcrumb.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { LayoutComponent } from './components/layout/layout.component';
 import { Error404Component } from './components/error404/error404.component';
 import { FeatherIconComponent } from './components/feather-icon/feather-icon.component';
